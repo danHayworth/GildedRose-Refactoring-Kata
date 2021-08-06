048445488043a2952d8c090be1e7ae19cadb99a1
Dan Hayworth <dan.hayworth@hotmail.com>
2021-08-07 01:21:31 +1200

    more refactoring on the switch cases

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -50,20 +50,14 @@ namespace csharp
                     {
                         item.Quality = item.Quality + 1;
 
-                        if (item.SellIn < 11)
+                        if (item.SellIn < 11 && item.Quality < 50)
                         {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
+                            item.Quality = item.Quality + 1;
                         }
 
-                        if (item.SellIn < 6)
+                        if (item.SellIn < 6 && item.Quality < 50)
                         {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
+                            item.Quality = item.Quality + 1;
                         }
                     }
                     item.SellIn = item.SellIn - 1;
@@ -82,6 +76,14 @@ namespace csharp
 
                     if (item.Quality > 0)
                     {
+                        item.Quality = item.Quality == 1 ? item.Quality - 1 : item.Quality - 2;
+
+                    }
+
+                    item.SellIn = item.SellIn - 1;
+
+                    if (item.SellIn < 0 && item.Quality > 0)
+                    {
                         if (item.Quality == 1)
                         {
                             item.Quality = item.Quality - 1;
@@ -90,25 +92,6 @@ namespace csharp
                         {
                             item.Quality = item.Quality - 2;
                         }
-
-                    }
-
-                    item.SellIn = item.SellIn - 1;
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Quality == 1)
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                            else
-                            {
-                                item.Quality = item.Quality - 2;
-                            }
-                        }
-
                     }
                     break;
 
@@ -120,12 +103,9 @@ namespace csharp
                     }
                     item.SellIn = item.SellIn - 1;
 
-                    if (item.SellIn < 0)
+                    if (item.SellIn < 0 && item.Quality > 0)
                     {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                     break;
             }
@@ -133,86 +113,4 @@ namespace csharp
     }
 }
 
-////////////////////////////              old code             \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-/*
-if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-{
-    if (item.Quality > 0)
-    {
-        if (item.Name != "Sulfuras, Hand of Ragnaros")
-        {
-            item.Quality = item.Quality - 1;
-
-            if (item.Name == "Conjured" && item.Quality > 0)
-            {
-                item.Quality = item.Quality - 1;
-            }
-        }
-    }
-}
-else
-{
-    if (item.Quality < 50)
-    {
-        item.Quality = item.Quality + 1;
-
-        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (item.SellIn < 11)
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-                }
-            }
-
-            if (item.SellIn < 6)
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-                }
-            }
-        }
-    }
-}
-
-if (item.Name != "Sulfuras, Hand of Ragnaros")
-{
-    item.SellIn = item.SellIn - 1;
-}
-
-if (item.SellIn < 0)
-{
-    if (item.Name != "Aged Brie")
-    {
-        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (item.Quality > 0)
-            {
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.Quality = item.Quality - 1;
-
-                    if (item.Name == "Conjured" && item.Quality > 0)
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-        }
-        else
-        {
-            item.Quality = item.Quality - item.Quality;
-        }
-    }
-    else
-    {
-        if (item.Quality < 50)
-        {
-            item.Quality = item.Quality + 1;
-        }
-    }
-}
-*/
 
