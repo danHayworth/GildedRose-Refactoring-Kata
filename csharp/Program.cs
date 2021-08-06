48190ca13782db878343aa36b304a0ae883a1cb8
Dan Hayworth <dan.hayworth@hotmail.com>
2021-08-06 16:35:49 +1200

    Checked all items in the list and passed the test

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -5,7 +5,7 @@ namespace csharp
 {
     public class Program
     {
-        static readonly IList<Item> Items = new List<Item>{
+        public static IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
--- csharpTests/GildedRoseTests.cs
+++ csharpTests/GildedRoseTests.cs
@@ -2,22 +2,23 @@
 using ApprovalTests.Combinations;
 using ApprovalTests.Reporters;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace csharp.Tests
 {
     [UseReporter(typeof(DiffReporter))]
+    [TestFixture]
     [TestClass()]
     public class GildedRoseTests
     {
         [TestMethod()]
         public void GildedRoseTest()
         {
-            IList<Item> items = new List<Item>() { new Item() { Name = "foo", Quality = 1, SellIn = 2 } };
+            IList<Item> items = new List<Item>(Program.Items);
             GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            //CombinationApprovals.VerifyAllCombinations<Item>();
-            Approvals.AssertEquals("foo", items[0].Name);
+            Approvals.VerifyAll<Item>(items, label: "");           
         }
 
         [TestMethod()]
