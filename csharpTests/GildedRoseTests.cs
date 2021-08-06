cb5bf75e81b1e09d6b1204859527e9a20e8a3745
Dan Hayworth <dan.hayworth@hotmail.com>
2021-08-06 16:07:30 +1200

    Created a unittest project and passed the first unittest for list

---
+++ csharpTests/GildedRoseTests.cs
@@ -0,0 +1,29 @@
+﻿using ApprovalTests;
+using ApprovalTests.Combinations;
+using ApprovalTests.Reporters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace csharp.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    [TestClass()]
+    public class GildedRoseTests
+    {
+        [TestMethod()]
+        public void GildedRoseTest()
+        {
+            IList<Item> items = new List<Item>() { new Item() { Name = "foo", Quality = 1, SellIn = 2 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            //CombinationApprovals.VerifyAllCombinations<Item>();
+            Approvals.AssertEquals("foo", items[0].Name);
+        }
+
+        [TestMethod()]
+        public void UpdateQualityTest()
+        {
+
+        }
+    }
+}
