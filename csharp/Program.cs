f9bc7326c07521266ae292fe4b3de988b38389d9
Dan Hayworth <dan.hayworth@hotmail.com>
2021-08-06 22:42:28 +1200

    kept only one conjured item

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -7,7 +7,6 @@ namespace csharp
     {
         public static IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Conjured", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
@@ -30,7 +29,6 @@ namespace csharp
                     SellIn = 5,
                     Quality = 49
                 },
-				// this conjured item does not work properly yet
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
         public static void Main(string[] args)
